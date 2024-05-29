96fcee16e96d4a1b7d033d9eb95ddef5309a2f1c
Stefano Scarpa <scarpastefano@icloud.com>
2024-05-29 13:15:40 +0200

    :add component navbar, footer, main :add route and view front-end

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function homepage(){
+        $article = Article::all();
+
+        return view('welcome', compact('article'));
+    }
+    
+    public function contacts(){
+        $article = Article::all();
+
+        return view('contacts', compact('article'));
+    }
+    
+    public function blog(){
+        $article = Article::all();
+
+        return view('blog', compact('article'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'homepage'])->name('homepage');
+Route::get('/contatti', [PageController::class, 'contacts'])->name('contacts');
+Route::get('/blog', [PageController::class, 'blog'])->name('blog');
+
